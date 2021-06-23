e3a2437c4f0df26e77ef8a2b475a5b2308b258f8
sjcco <73748496+sjcco@users.noreply.github.com>
2021-06-23 12:43:42 -0500

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
 module.exports = {
   mode: "development",
+  devtool: false,
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
   }
 }
